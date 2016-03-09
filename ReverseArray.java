e824238a4b56342674221b6f4beaccc3be7f9349
Madhuash <rao.madhavi57@gmail.com>
2016-03-09 17:34:01 +0530

    Added files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,10 @@
+
+public class ReverseArray {
+	  public static void main(String[] args) {
+	        int var[]={4,6,7,8};
+	        int i;
+	        for(i =3; i>=0; i--){
+	            System.out.println(var[i]);
+	        }
+	    }
+}
